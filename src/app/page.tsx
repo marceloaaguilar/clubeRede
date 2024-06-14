c675307a55be06fd6060f400f3361af4bb88b805
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2024-06-14 00:01:52 -0300

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,141 @@
+'use client'
 import Image from "next/image";
+import 'animate.css';
+import { useState } from "react";
 
 export default function Home() {
+  const[showMenu, setShowMenu] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="scroll-smooth">
+       <nav className="flex justify-between items-center w-[92%]  mx-auto">
+            <div>
+              <Image src="/logo_Clube_Rede.png" className="m-5" alt="Logo Clube Rede" width={100} height={50}></Image>  
+            </div>
+            <div data-collapse="collapse" className={`${showMenu == true? '': "hidden"} transition-all duration-400 ease-in-out justify-center text-center lg:block bg-black nav-links md:static absolute md:min-h-fit min-h-[100vh]  left-0 top-[15%] md:w-auto  w-full flex items-center px-5`}>
+                <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+                  <li>
+                    <a className="text-2xl lg:text-lg hover:text-red-700" href="#home">Início</a>
+                  </li>
+                  <li>
+                    <a className="text-2xl lg:text-lg hover:text-red-700" href="#sobre">Conheça o Clube</a>
+                  </li>
+                  <li>
+                      <a className="text-2xl lg:text-lg hover:text-red-700" href="#parceiros">Nossos Parceiros</a>
+                  </li>
+                  <li>
+                      <a className="text-2xl lg:text-lg hover:text-red-700" href="#descontos">Descontos</a>
+                  </li>
+                  <li>
+                    <button className="text-2xl lg:text-lg bg-red-700 text-white px-10 py-2 rounded-full hover:bg-red-900 mt-2">Ver Descontos</button>
+                  </li>
+                  <li>
+                    <button className="text-2xl lg:text-lg bg-transparent border-2 border-red-700 text-white px-5 py-2 rounded-full hover:bg-red-800 hover:border-white">Área do Associado</button>
+                  </li>
+                </ul>
+            </div>
+            <div className="flex items-center gap-6">
+              <button onClick={() => setShowMenu(!showMenu)} type="button" className={`md:hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`} aria-controls="navbar-default" aria-expanded="false">
+                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+              </button>
+            </div>
+        </nav>
+
+      <div id="home" className="flex items-center justify-start h-screen" style={{background: "url('/family.jpg')",  width: '100%', objectFit: 'cover',backgroundPosition: '60% 50%',  backgroundRepeat: 'no-repeat'}}>
+        <div className="p-6 ml-6">
+          <h3 className="text-2xl font-bold">Bem-vindo ao Clube de Descontos Rede Veículos!</h3>
+          <p>Descontos que fazem você acelerar!</p>
+          <button className="bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 mt-4 w-1/2">Ver Mais</button>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div id="sobre"className="flex-col items-center justify-center bg-black-500 p-10">
+        <div className="flex-col bg-red-800 mt-4 p-8 rounded text-center">
+          <h4 className="text-4xl font-semibold mt-4">Como funciona o Clube</h4>
+          <div className="flex flex-wrap items-center justify-center pb-5">
+            <div className="flex-col items-center justify-center p-6 text-center w-72 h-56 animate__animated animate__zoomInLeft wow">
+              <div className="flex items-center text-red-700">
+                <div className="bg-black mt-5 mb-4 text-6xl rounded-full w-full">01</div>
+              </div>
+              <h4 className="text-lg font-bold">Participe</h4>
+              <p>Faça parte do nosso Clube e tenha acesso aos benefícios. É prático!</p>
+            </div>
+
+            <div className="flex-col items-center justify-center p-6 text-center w-72 h-56 animate__animated animate__zoomInLeft wow">
+              <div className="flex items-center text-red-700">
+                <div className="bg-black mt-5 mb-4 text-6xl rounded-full w-full">02</div>
+              </div>
+              <h4 className="text-lg font-bold">Escolha</h4>
+              <p>São centenas de benefícios que você pode escolher e usar quantas vezes quiser.</p>
+            </div>
+
+            <div className="flex-col items-center justify-center p-6 text-center w-72 h-56 animate__animated animate__zoomInLeft wow">
+              <div className="flex items-center text-red-700">
+                <div className="bg-black mt-5 mb-4 text-6xl rounded-full w-full">03</div>
+              </div>
+              <h4 className="text-lg font-bold">Apresente</h4>
+              <p>Depois de escolher o benefício que você quer, é só aproveitar.</p>
+            </div>
+
+            <div className="flex-col items-center justify-center p-6 text-center w-72 h-56 animate__animated animate__zoomInLeft wow rounded">
+              <div className="flex items-center text-red-700">
+                <div className="bg-black mt-5 mb-4 text-6xl rounded-full w-full">04</div>
+              </div>
+              <h4 className="text-lg font-bold">Economize</h4>
+              <p>Pronto! Você já está economizando. Use quantas vezes quiser e economize todo dia.</p>
+            </div>
+      
+          </div>
+
+        </div>
+      </div>
+      
+      <div id="parceiros" className="flex-col bg-white w-100 mt-4 p-6 h-full text-center">
+        <h4 className="text-black text-4xl mt-5">Grandes marcas,<b> grandes descontos</b>!</h4>
+        <div className="grid xl:grid-cols-6 gap-4 mt-9 justify-center w-full">
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/488.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clubemarisa.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/ponto_pontofrio.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clubeDrogasil.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/Aliexpress_logo.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clube753.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/djsnlcisjvisjncisdp.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/ligoShoestock.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/logoMovida250x250px.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clube-348.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/vivara-logo.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clube-422.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clubeDrogaRaia.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clube-341.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clube-casasbahia.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/clubemagalu.jpg" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/LogoPetzCBC250x250.png" alt="logoCineart" width={350} height={350}></Image>
+            <Image className="hover:p-3  transition-all w-48 h-48 object-contain" src="/Dominos.jpg" alt="logoCineart" width={350} height={350}></Image>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div id="descontos" className="flex-col flex bg-zinc-100 pb-10">
+        <div className="w-full flex flex-col justify-center p-8">
+          <div className="grid xl:grid-cols-3 gap-4 items-center mt-8 xl:p-6">
+            <h4 className="text-4xl  font-bold text-black mb-2">Descontos Imperdíveis para você aproveitar.</h4>
+            <Image src="/partners.jpg" className="w-full col-span-1 hover:p-3 transition-all" style={{borderRadius: '20px'}} alt="Pizza Dominos" width={300} height={100}></Image>
+            <Image src="/loja.jpg" className="w-full col-span-1 hover:p-3 transition-all" alt="Pizza Dominos" width={300} height={100}></Image>
+          </div>
+          <div className="grid xl:grid-cols-3 gap-4 items-center mt-8 xl:p-6">
+            <Image src="/womanSmartphone.jpg" className="w-full object-cover h-80 col-span-1 hover:p-3 transition-all" style={{borderRadius: '20px'}} alt="Pizza Dominos" width={300} height={100}></Image>
+            <Image src="/cinema.jpg" className="w-full object-cover col-span-1 h-80 hover:p-3 transition-all" style={{borderRadius: '20px'}} alt="Pizza Dominos" width={300} height={100}></Image>
+            <Image src="/childrenNotebook.jpg" className="w-full object-cover h-80 col-span-1 hover:p-3 transition-all" style={{borderRadius: '20px'}} alt="Pizza Dominos" width={300} height={100}></Image>
+          </div>
+        </div>
+          <div className="w-full justify-center flex mt-5">
+            <button className="bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 mt-4 w-56">Ver Descontos</button>
+          </div>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex justify-center text-center gap-10 bg-black items-center h-52" >
+        <Image src="/logo_Clube_Rede.png" alt="Logo Clube Rede" width={100} height={50}></Image>
+        <p>Central de Descontos <br/> 0800 940 2195</p>
       </div>
     </main>
   );
