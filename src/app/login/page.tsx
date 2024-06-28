fd5dc97c041a81b7945c0c870962eb10d3c28d76
marceloaaguilar <marcelodalmeida42@gmail.com.br>
2024-06-28 12:48:35 -0300

    Adicionando área do associado

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,9 +17,6 @@ export default function Login(){
                 <div>
                     <div className="flex items-center justify-between">
                     <label className="block text-sm font-medium leading-6 text-white">Senha</label>
-                    {/* <div className="text-sm">
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                    </div> */}
                     </div>
                     <div className="mt-2">
                     <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
