580b196c968118e2746e5637b0d360ad209677e4
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-07-17 23:08:03 -0300

    fix: removing static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: { missingSuspenseWithCSRBailout: false },
-    output: 'export',
+    // output: 'export',
     reactStrictMode: false
 };
 
